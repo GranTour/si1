1e9139feeb954d0fb785acf968a5a893b71506ef
Pablicio Bernardo do Amaral 20821026 <pablicioba@lcc2-03.lcc.ufcg.edu.br>
2013-07-05 16:56:19 -0300

    Arquivos adicionados

---
+++ lab1/ufcg.si1/src/TestaXpto.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class TestaXpto {
+
+	@Test
+	public void testaXpto() {
+		org.junit.Assert.assertEquals( "Funcionando?",Xpto.xpto() );
+	}
+
+}
---
+++ lab1/ufcg.si1/src/Xpto.java
@@ -0,0 +1,13 @@
+
+public class Xpto {
+
+
+	public static void main(String[] args) {
+		xpto();
+
+	}
+	
+	public static String xpto(){
+		 return "Funcionando?";
+	}
+}
